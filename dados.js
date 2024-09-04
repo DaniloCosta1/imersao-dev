d9ef222af6833ca925589783e5fce1c25ff200e2
Danilo Costa <99129932+DaniloCosta1@users.noreply.github.com>
2024-09-04 09:54:51 -0300

    ok

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a 'Fadinha', é uma das maiores promessas do skate mundial. Com apenas [idade] anos, ela já conquistou diversos títulos e se tornou um ícone da nova geração de skatistas. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é um nome que brilha no cenário esportivo mundial. A ginasta brasileira, com sua força, agilidade e determinação, conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional. Bicampeã olímpica, Rebeca acumula diversas medalhas e recordes, sendo a maior medalhista olímpica da história do Brasil. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um símbolo de força e empoderamento para jovens atletas e para toda a sociedade.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma medalhista olímpica brasileira de judô. Sua conquista histórica nos Jogos Olímpicos de Paris a tornou um exemplo de superação e determinação. Sua trajetória inspiradora, marcada por desafios e conquistas, a coloca como um dos maiores nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki.org/wiki/Beatriz_Souza"
+    }
+];
+
+
+
+
+console.log(dados[0].titulo)
