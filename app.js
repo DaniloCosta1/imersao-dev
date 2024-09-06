246eeb4eacc86464228df5dd9e9c8121f68373dc
Danilo Costa <99129932+DaniloCosta1@users.noreply.github.com>
2024-09-06 10:14:34 -0300

    ok

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 function pesquisar(){
     let section = document.getElementById('resultados-pesquisa')
     let resultado = '';
-    for(let item of dados){
-        resultado += `<div class="item-resultado">
-                    <h2><a href=${item.link} target="_blank">${item.titulo}</a></h2>
-                    <p class="descricao-meta">${item.descricao}</p>
-                    <a href=${item.link} target="_blank" >mais sobre ${item.titulo}</a>
-                </div>`
-    }
-    section.innerHTML = resultado
-}
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toUpperCase();
 
+    if(!campoPesquisa){
+        section.innerHTML = "Nada encontrado. Digite algo para pesquisar"
+        return
+    }
+    
+    for(let item of dados){
+        if(item.titulo.toUpperCase().includes(campoPesquisa) || item.descricao.toUpperCase().includes(campoPesquisa)
+            || item.tags.toUpperCase().includes(campoPesquisa) ){
+            resultado += `<div class="item-resultado">
+                            <h2><a href=${item.link} target="_blank">${item.titulo}</a></h2>
+                            <p class="descricao-meta">${item.descricao}</p>
+                            <a href=${item.link} target="_blank" >mais sobre ${item.titulo}</a>
+                        </div>`;             
+        }
+    }
+
+    if(!resultado){
+        resultado = "Nada foi encontrado";
+    }
+    section.innerHTML = resultado;
+}
